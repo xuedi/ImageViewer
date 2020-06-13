71a1e02da7a8307366ede3e191fb548dbff6681c
xuedi <xuedi@beijingcode.org>
2020-06-13 19:46:05 +0200

    added vue with vuetify for the basic design layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+
+    <v-app-bar clipped-left dense app color="primary" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <div class="d-flex align-center">
+        Gallery
+      </div>
+      <v-spacer></v-spacer>
+      <div class="d-flex align-center">
+        <v-text-field dense flat solo-inverted hide-details prepend-inner-icon="mdi-magnify" label="Search"
+                      class="hidden-sm-and-down"></v-text-field>
+      </div>
+      <v-spacer></v-spacer>
+      <div class="d-flex align-center">
+        Layout:
+        <v-icon>mdi-view-list</v-icon>
+        <v-icon>mdi-view-module</v-icon>
+        <v-icon>mdi-view-comfy</v-icon>
+      </div>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" clipped app>
+      <Navigation/>
+    </v-navigation-drawer>
+
+    <v-content>
+      <Gallery/>
+    </v-content>
+
+    <v-bottom-navigation fixed dark>
+      Selected tags and other stuff
+    </v-bottom-navigation>
+
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Gallery from './components/Gallery'
+  import Navigation from './components/Navigation'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+
+    components: {
+      Gallery,
+      Navigation
+    },
+
+    data: () => ({
+      drawer: null,
+    })
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ frontend/src/components/Navigation.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-container class="mx-auto" width="300">
+
+
+    <v-list>
+
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Home</v-list-item-title>
+      </v-list-item>
+
+
+
+
+      <v-list-group no-action sub-group value="true">
+
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title>Countries</v-list-item-title>
+          </v-list-item-content>
+        </template>
+
+        <v-list-item v-for="country in countries" :key="country.name" link >
+          <v-list-item-title v-text="country.name"></v-list-item-title>
+          <v-list-group no-action sub-group value="true">
+
+            <v-list-item v-for="event in events" :key="event.name" link >
+              <v-list-item-title v-text="event.name"></v-list-item-title>
+            </v-list-item>
+
+          </v-list-group>
+        </v-list-item>
+
+      </v-list-group>
+
+    </v-list>
+
+
+
+
+    <v-expansion-panels accordion focusable>
+      <v-expansion-panel v-for="country in countries" :key="country.name">
+        <v-expansion-panel-header v-text="country.name"></v-expansion-panel-header>
+        <v-expansion-panel-content>
+          Eventlist
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
+
+
+
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+
+  data: () => ({
+    drawer: null,
+    countries: [
+      {name: 'China'},
+      {name: 'Czech'},
+      {name: 'Denmark'},
+      {name: 'England'},
+      {name: 'France'},
+      {name: 'Germany'},
+      {name: 'Greece'},
+      {name: 'Ireland'},
+      {name: 'Italy'},
+      {name: 'Netherlands'},
+      {name: 'Poland'},
+      {name: 'Romania'},
+      {name: 'Serbia'},
+    ],
+    menu: [
+      {name: 'China'},
+    ],
+  })
+}
+</script>
